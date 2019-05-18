09de7bc0157cd46178da4d033a7b6428f67f5629
Junior <jtavarez8@gmail.com>
2019-05-18 19:18:58 -0400

    update

---
+++ build/precache-manifest.75b9abc0483eac96ddd8228ed5967169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c1f22df0e6fc28b260462b6f9f0cf30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a1e6432959d028d44976",
+    "url": "/static/css/main.224a4d9a.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "a1e6432959d028d44976",
+    "url": "/static/js/main.29b6daa0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
