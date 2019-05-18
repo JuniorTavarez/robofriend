8be27088d5004bf8da7228f4de8b78ff82054ca7
Junior <jtavarez8@gmail.com>
2019-05-18 09:18:31 -0400

    updated react app with style and search bar working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CardList from './CardList'
-import SeachBox from './SearchBox'
 import { robots } from './robots'
 import SearchBox from './SearchBox'
+import './App.css'
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfields: ''
+    }
+  }
+  onSearchChange = event => {
+    this.setState({ searchfields: event.target.value })
+  }
+  render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfields.toLowerCase())
+    })
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    )
+  }
 }
 
 export default App
